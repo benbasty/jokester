f2a807c9f9ac76801e3b8ad1bd68c90115fc8cd5
benbasty <benbasty@yahoo.fr>
2024-01-30 14:47:01 +0800

    added figlet npm package

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 var giveMeAJoke = require('give-me-a-joke');
 var colors = require('colors');
+var figlet = require("figlet");
 //lets get a random dad joke
+var name = "ben";
 const getAjoke = giveMeAJoke.getRandomDadJoke((joke) =>{
     console.log(joke.rainbow);
 });
+figlet(name, function (err, data) {
+    if (err) {
+      console.log("Something went wrong...");
+      console.dir(err);
+      return;
+    }
+    console.log(data.green);
+  });
